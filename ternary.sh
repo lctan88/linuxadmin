64460b2837e45f78ee415200f12796dec06c3464
lctan88 <leongchye.tan@gmail.com>
2023-11-12 14:00:08 +0800

    Modified source

---
+++ ternary.sh
@@ -0,0 +1,12 @@
+if [ $# -ne 2 ]
+    then 
+        echo " Required arguments not supplied"
+        exit
+fi
+
+a=$1; b=$2
+echo "first param: $a"
+echo "second param: $b"
+
+z=$((a > b ? a : b))
+echo "$z" >> /tmp/a.log
